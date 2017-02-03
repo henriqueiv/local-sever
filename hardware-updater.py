e217d5eb48046f192f960c6dbba4635fac02e8c1
William Hass <wm.hass@gmail.com>
2017-02-03 14:10:31 -0200

    Saving in DB

--- hardware-updater.py
+++ hardware-updater.py
@@ -1,6 +1,9 @@
 import smbus
 import time
 
+import pymongo
+from pymongo import MongoClient
+
 def StringToBytes(val):
   retVal = []
   for c in val:
@@ -19,7 +22,16 @@ address = 0x04
 
 #bus.write_i2c_block_data(address, 0, StringToBytes("Hello World"))
 
+client = MongoClient('localhost', 27017)
+db = client.test_database
+data_log = db.data_log
+
 while True:
 	bytes = bus.read_i2c_block_data(address, 0)
-	print "".join(map(chr, bytes)).strip("\xff")
+
+	data = "".join(map(chr, bytes)).strip("\xff")
+	data_log.insert_one({"data": data})
+
+
+	print data
 	time.sleep(1)
