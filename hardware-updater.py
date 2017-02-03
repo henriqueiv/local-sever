4083d81ae6d89c60d88e0dcd354c4ab52430fb1a
William Hass <wm.hass@gmail.com>
2017-02-03 14:17:15 -0200

    Inserting items

--- hardware-updater.py
+++ hardware-updater.py
@@ -23,14 +23,24 @@ address = 0x04
 #bus.write_i2c_block_data(address, 0, StringToBytes("Hello World"))
 
 client = MongoClient('localhost', 27017)
-db = client.test_database
+db = client['420bits']
 data_log = db.data_log
 
 while True:
 	bytes = bus.read_i2c_block_data(address, 0)
 
 	data = "".join(map(chr, bytes)).strip("\xff")
-	data_log.insert_one({"data": data})
+
+	items = data.split("|")
+
+	humidity = items[0]
+	temperature = items[1]
+	co2 = items[3]
+	ts = time.time()
+
+	data_log.insert_one({"timestamp": ts, "type": 0, "value": humidity})
+	data_log.insert_one({"timestamp": ts, "type": 1, "value": temperature})
+	data_log.insert_one({"timestamp": ts, "type": 2, "value": co2})
 
 
 	print data
