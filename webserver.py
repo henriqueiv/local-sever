ea5abe75bec4152f8df811702bc984a57c890707
William Hass <wm.hass@gmail.com>
2017-02-03 23:40:35 -0200

    Webserver

---
+++ webserver.py
@@ -0,0 +1,80 @@
+from tornado import websocket, web, ioloop
+import json
+import time
+import os
+import pymongo
+from pymongo import MongoClient
+
+cl = []
+bus = smbus.SMBus(1)
+address = 0x04
+
+client = MongoClient('localhost', 27017)
+db = client['420bits']
+data_log = db.data_log
+
+def StringToBytes(val):
+  retVal = []
+  for c in val:
+    retVal.append(ord(c))
+  return retVal
+
+def readBus():
+  data = ""
+  for i in range(0, 1):
+          data += chr(bus.read_byte(address));
+  print data
+
+def notify_all_clients(data):
+    for c in cl:
+        c.write_message(data)
+
+class SocketHandler(websocket.WebSocketHandler):
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+    def on_message(self, message):
+        print "Received messaged: " + message
+
+        if message == "read":
+            print "just read"
+        else:
+            try:
+                receivedObject = json.loads(message)
+                if receivedObject.has_key("timer_interval"):
+                    print "aa"
+
+            except:
+                print("error parsing message:" + str(message))
+
+
+class TimerHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def post(self):
+        pass
+
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+        print "Received get request"
+
+app = web.Application([
+    (r'/ws', SocketHandler),
+    (r'/timer', TimerHandler),
+    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
+    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
+])
+
+if __name__ == '__main__':
+    app.listen(8888)
+    ioloop.IOLoop.instance().start()
