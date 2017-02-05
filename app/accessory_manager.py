86354d15fff32e71f7518b13fc69b4a0bf991a27
William Hass <wm.hass@gmail.com>
2017-02-05 03:24:59 -0200

    Debugging

--- app/accessory_manager.py
+++ app/accessory_manager.py
@@ -33,7 +33,10 @@ class ArduinoAccessories:
 
 	def turn_on_accessory(self, accessory_id):
 		message = accessory_id + "1"
+		print "message"
+		print message
 		self.i2cbus.write_i2c_block_data(self.address, 0, StringToBytes(message))
+		print "aaa"
 
 	def turn_off_accessory(self, accessory_id):
 		message = accessory_id + "0"
