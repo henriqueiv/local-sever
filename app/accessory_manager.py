df2327e99156723cc7814b2e40c476cf4dd0fde1
William Hass <wm.hass@gmail.com>
2017-02-05 03:17:16 -0200

    Debugging

--- app/accessory_manager.py
+++ app/accessory_manager.py
@@ -30,7 +30,10 @@ class ArduinoAccessories:
 			Accessory("CO2",DefaultCO2AccessoryID, AccessoryTypeCO2, co2)
 		]
 		return accessories
-		
+
+	def turn_on_accessory(self):
+		print "Hello"
+
 	# def turn_on_accessory(self,accessory_id):
 	# 	message = accessory_id + "1"
  #        self.i2cbus.write_i2c_block_data(self.address, 0, StringToBytes(message))
