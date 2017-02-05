88d10163010020244bac3f5d5405ec18d1c96b97
William Hass <wm.hass@gmail.com>
2017-02-05 13:11:25 -0200

    Refactoring

--- webserver.py
+++ webserver.py
@@ -24,34 +24,30 @@ class SocketHandler(websocket.WebSocketHandler):
     def on_message(self, message):
         print "Received messaged: " + message
 
-        try:
-            receivedObject = json.loads(message)
-            if not receivedObject.has_key("action"):
-                print "Key action not found"
-                return
+        socket_message = SocketMessage(message)
 
-            action = receivedObject["action"]
+        if socket_message.action is None:
+            print "Error: Action property not received in message: " + message
+            return
 
-            if action == "turn_on":
-                deviceToTurnOn = receivedObject["id"]
-                self.accessory_manager.turn_on_accessory(deviceToTurnOn)
-                self.update_all_clients(self.accessory_manager.get_accessories_json())
 
-                print "Turn on: " + str(deviceToTurnOn)
+        if socket_message.action == "turn_on" and socket_message.id is not None:
+            deviceToTurnOn = socket_message.id
+            self.accessory_manager.turn_on_accessory(deviceToTurnOn)
+            self.update_all_clients(self.accessory_manager.get_accessories_json())
 
-            elif action == "turn_off":
-                deviceToTurnOff = receivedObject["id"]
-                self.accessory_manager.turn_off_accessory(deviceToTurnOff)
-                self.update_all_clients(self.accessory_manager.get_accessories_json())
+            print "Turn on: " + str(deviceToTurnOn)
 
-                print "Turn off: " + str(deviceToTurnOff)
+        elif socket_message.action == "turn_off" and socket_message.id is not None:
+            deviceToTurnOff = socket_message.id
+            self.accessory_manager.turn_off_accessory(deviceToTurnOff)
+            self.update_all_clients(self.accessory_manager.get_accessories_json())
 
-            elif action == "read":
-                self.update_self_client(self.accessory_manager.get_accessories_json())
-                print "Read"
+            print "Turn off: " + str(deviceToTurnOff)
 
-        except:
-            print("error parsing message:" + str(message))
+        elif socket_message.action == "read":
+            self.update_self_client(self.accessory_manager.get_accessories_json())
+            print "Read"
 
     def update_all_clients(self, object):
         data = json.dumps(object)
@@ -61,6 +57,28 @@ class SocketHandler(websocket.WebSocketHandler):
     def update_self_client(self, object):
         data = json.dumps(object)
         self.write_message(data)
+
+
+def SocketMessage:
+    action = None
+    id = None
+
+    def __init__(self, socket_message):
+        try:
+            message_object = json.loads(socket_message)
+
+            if message_object.has_key("action"):
+                self.action = message_object["action"]
+
+            if message_object.has_key("id"):
+                self.id = message_object["id"]
+
+        except:
+            print("error parsing message:" + str(message))
+            return
+
+
+
 
 
 class TimerHandler(web.RequestHandler):
