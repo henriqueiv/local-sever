5b7d62935df2b5571cbaef7b5fcd595712a8ea81
William Hass <wm.hass@gmail.com>
2017-02-05 11:23:31 -0200

    Refactoring

--- hardware-updater.py
+++ hardware-updater.py
@@ -15,29 +15,51 @@ accessory_manager = AccessoryManager()
 class AccessoryFactory:
 	client = None
 	db = None
+	table = None
 
 	def __init__(self):
 		self.client = MongoClient('localhost', 27017)
 		self.db = client['420bits']
+		self.table = self.db.accessories
 
 	def insert_or_update(self, accessory):
 		accessory_dictionary = accessory.to_db_json()
 
-		accessories_db = self.db.accessories
-		accessories_db.update({"_id": accessory.id}, accessory_dictionary,True)
+		self.table.update({"_id": accessory.id}, accessory_dictionary,True)
+
+class AccessoryLog:
+	accessory = None
+	timestamp = 0
+
+	def __init__(self, accessory, timestamp):
+		self.accessory = accessory
+		self.timestamp = timestamp
+
+class AccessoryLogFactory:
+	client = None
+	db = None
+	table = None
+
+	def __init__(self):
+		self.client = MongoClient('localhost', 27017)
+		self.db = client['420bits']
+		self.table = self.db.data_log
+
+	def insert(accessory_log):
+		data_log.insert_one({"timestamp": accessory_log.timestamp, "accessory": accessory_log.accessory.to_db_json()})
+
 
 accessory_factory = AccessoryFactory()
+accessory_log_factory = AccessoryLogFactory()
 
 while True:
 	
 	ts = time.time()
 	accessories = accessory_manager.get_accessories()
-
 	for accessory in accessories:
-		accessory_dictionary = accessory.to_db_json()
 
 		accessory_factory.insert_or_update(accessory)
-		data_log.insert_one({"timestamp": ts, "accessory": accessory_dictionary})
+		accessory_log_factory.insert(AccessoryLog(accessory, ts))
 
 		print accessory
 
