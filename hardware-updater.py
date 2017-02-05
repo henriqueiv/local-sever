6c572578cca133cdb499c40585b58ab7668d34c0
William Hass <wm.hass@gmail.com>
2017-02-05 11:12:04 -0200

    Refactoring

--- hardware-updater.py
+++ hardware-updater.py
@@ -12,22 +12,27 @@ accessories_db = db.accessories
 
 accessory_manager = AccessoryManager()
 
+class AccessoryFactory:
+	client = None
+	db = None
+
+	def __init__(self):
+		self.client = MongoClient('localhost', 27017)
+		self.db = client['420bits']
+
+factory = AccessoryFactory()
+
 while True:
 	
 	ts = time.time()
 	accessories = accessory_manager.get_accessories()
 
 	for accessory in accessories:
-		accessory_dictionary = accessory.to_json()
+		accessory_dictionary = accessory.to_db_json()
 
 		accessories_db.update({"_id": accessory.id}, accessory_dictionary,True)
 		data_log.insert_one({"timestamp": ts, "accessory": accessory_dictionary})
 
 		print accessory
 
-	# data_log.insert_one({"timestamp": ts, "type": AccessoryTypeHumidity, "value": humidity, "accessory": accessories.find_one({"_id": DefaultHumidityAccessoryID})})
-	# data_log.insert_one({"timestamp": ts, "type": AccessoryTypeTemperature, "value": temperature, "accessory": accessories.find_one({"_id": DefaultTemperatureAccessoryID})})
-	# data_log.insert_one({"timestamp": ts, "type": AccessoryTypeCO2, "value": co2, "accessory": accessories.find_one({"_id": DefaultCO2AccessoryID})})
-
-	# print data
 	time.sleep(30)
