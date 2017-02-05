3b12df0381eb90d466b6786789116e007d3300bc
William Hass <wm.hass@gmail.com>
2017-02-05 02:09:33 -0200

    Debugging

--- test.py
+++ test.py
@@ -1,15 +1,6 @@
 import models
 import smbus
-import lib.accessory_manager
-
-AccessoryTypeUndefined = -1
-AccessoryTypeHumidity = 0
-AccessoryTypeTemperature = 1
-AccessoryTypeCO2 = 2
-
-DefaultHumidityAccessoryID = 1
-DefaultTemperatureAccessoryID = 2
-DefaultCO2AccessoryID = 3
+import app.accessory_manager
 
 
 
@@ -17,8 +8,5 @@ manager = AccessoryManager()
 manager.name = "william"
 
 print manager.name
-
-accessory = Accessory("Will","0",AccessoryTypeTemperature)
-print accessory.name
 
 print manager.get_accessories()
