f5993d644f17b557c22f41f24ebeda6a39455500
William Hass <wm.hass@gmail.com>
2017-02-07 10:29:45 -0200

    Finished Timer

--- webserver.py
+++ webserver.py
@@ -99,13 +99,13 @@ class NotesHandler(web.RequestHandler):
             #if "accessory_log_id" not in json_object:
                 #errors.append({"message": "accessory_log_id field not found"})
 
-            text = json_object["text"]
-            if not text:
-                errors.append({"message": "text field can not be empty"})
+            # text = json_object["text"]
+            # if not text:
+            #     errors.append({"message": "text field can not be empty"})
 
-            accessory_log_id = json_object["accessory_log_id"]
-            if not accessory_log_id:
-                errors.append({"message": "accessory_log_id field can not be empty"})                
+            # accessory_log_id = json_object["accessory_log_id"]
+            # if not accessory_log_id:
+            #     errors.append({"message": "accessory_log_id field can not be empty"})                
 
             if errors.size:
                 self.write(json.dumps({"errors": errors}))
