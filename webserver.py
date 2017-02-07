c86d118421ad962a2d0da9994c6768bd4ebb956e
William Hass <wm.hass@gmail.com>
2017-02-07 09:48:38 -0200

    Finished Timer

--- webserver.py
+++ webserver.py
@@ -86,7 +86,7 @@ class AccessoryLogsHandler(web.RequestHandler):
     def post(self):
         print "Action parameter:" + self.get_argument("action", "")
 
-        self.write("Headers: " + str(self.headers))
+        self.write("Headers: " + str(self.request.headers))
         self.write("{}")
         self.write("<br>")
         self.finish()
