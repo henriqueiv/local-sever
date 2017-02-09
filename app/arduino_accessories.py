8f8aa6304aae94cde661a0f84bb850c9d36c8076
William Hass <wm.hass@gmail.com>
2017-02-09 00:12:55 -0200

    Cast id to string

--- app/arduino_accessories.py
+++ app/arduino_accessories.py
@@ -32,9 +32,9 @@ class ArduinoAccessories:
 		return accessories
 
 	def turn_on_accessory(self, accessory_id):
-		message = accessory_id + "1"
+		message = str(accessory_id) + "1"
 		self.i2cbus.write_i2c_block_data(self.address, 0, StringToBytes(message))
 
 	def turn_off_accessory(self, accessory_id):
-		message = accessory_id + "0"
+		message = str(accessory_id) + "0"
 		self.i2cbus.write_i2c_block_data(self.address, 0, StringToBytes(message))
