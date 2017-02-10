8db54d454d2ae10dc2b57200a19af4d84d6c9ce1
William Hass <wm.hass@gmail.com>
2017-02-10 11:16:08 -0200

    Fixed class not defined

---
+++ app/accessory_logger.py
@@ -0,0 +1,14 @@
+class AccessoryLogger:
+
+	accessory_factory = AccessoryFactory()
+	accessory_log_factory = AccessoryLogFactory()
+	accessory_manager = AccessoryManager()
+
+	def log(self):
+		timestamp = time.time()
+		accessories = self.accessory_manager.get_accessories()
+		for accessory in accessories:
+			self.accessory_factory.insert_or_update(accessory)
+			self.accessory_log_factory.insert(AccessoryLog(accessory, timestamp))
+			print accessory
+		pass
--- data-logger.py
+++ data-logger.py
@@ -3,25 +3,11 @@ from app.accessory_manager import AccessoryManager
 from app.factories import AccessoryFactory, AccessoryLogFactory, TimerTaskFactory
 from app.models import AccessoryLog
 from app.task_manager import TaskManager
+from accessory_logger import AccessoryLogger
 
 task_manager = TaskManager()
 timer_task_factory = TimerTaskFactory()
 accessory_logger = AccessoryLogger()
-
-class AccessoryLogger:
-
-	accessory_factory = AccessoryFactory()
-	accessory_log_factory = AccessoryLogFactory()
-	accessory_manager = AccessoryManager()
-
-	def log(self):
-		timestamp = time.time()
-		accessories = self.accessory_manager.get_accessories()
-		for accessory in accessories:
-			self.accessory_factory.insert_or_update(accessory)
-			self.accessory_log_factory.insert(AccessoryLog(accessory, timestamp))
-			print accessory
-		pass
 
 
 while True:
