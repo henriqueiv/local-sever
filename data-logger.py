7ee3982b3e590ec43c797393229f9e20c47fb2c7
William Hass <wm.hass@gmail.com>
2017-02-10 11:17:02 -0200

    fixing package name

--- data-logger.py
+++ data-logger.py
@@ -3,7 +3,7 @@ from app.accessory_manager import AccessoryManager
 from app.factories import AccessoryFactory, AccessoryLogFactory, TimerTaskFactory
 from app.models import AccessoryLog
 from app.task_manager import TaskManager
-from accessory_logger import AccessoryLogger
+from app.accessory_logger import AccessoryLogger
 
 task_manager = TaskManager()
 timer_task_factory = TimerTaskFactory()
