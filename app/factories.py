f3c30a3e87e968268fd281a8c28ae165a932b936
William Hass <wm.hass@gmail.com>
2017-02-11 16:20:25 -0200

    rying to update taks

--- app/factories.py
+++ app/factories.py
@@ -30,7 +30,7 @@ class TimerTaskFactory(AbstractFactory):
 	def insert(self, timer_task):
 		to_save = timer_task.mongo_json_representation()
 		to_save["_id"] = ObjectId(to_save["_id"])
-		return self.table.insert_one(to_save).inserted_id
+		return self.table.update(to_save,{"upsert": True}).inserted_id
 
 	def delete(self, task_id):
 		result = self.table.delete_many({"_id": ObjectId(task_id)})
