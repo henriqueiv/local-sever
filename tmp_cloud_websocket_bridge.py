9043ca4055f2d60274006cd5f2720c173d4d63d7
William Hass <wm.hass@gmail.com>
2017-02-12 19:33:17 -0200

    refactoring tmp client

--- tmp_cloud_websocket_bridge.py
+++ tmp_cloud_websocket_bridge.py
@@ -1,11 +1,9 @@
 import websocket
-import threading
 import thread
 import time
 
 def on_message(ws, message):
-	print "Received message: " + str(message)
-	print message
+    print "Message: " + str(message)
 
 def on_error(ws, error):
     print error
@@ -15,23 +13,17 @@ def on_close(ws):
 
 def on_open(ws):
     def run(*args):
-    	ws.send("{\"register\": \"aaa\"}")
-        #ws.close()
+        for i in range(3):
+            ws.send("{\"register\": \"aaa\"}")
+        print "thread terminating..."
     thread.start_new_thread(run, ())
 
+
 if __name__ == "__main__":
-	ws = websocket.WebSocketApp("ws://ec2-52-34-138-21.us-west-2.compute.amazonaws.com:8888/websocket",on_message=on_message,on_error=on_error,on_close=on_close)
-	#ws.enableTrace(True)
-	#ws = websocket.WebSocketApp("ws://127.0.0.1:8888/ws",on_message=on_message,on_error=on_error,on_close=on_close)
-	ws.on_open = on_open
-
-	wst = threading.Thread(target=ws.run_forever)
-	wst.daemon = True
-	wst.start()
-
-	while True:
-		if ws is not None:
-			ws.send("{\"from_device\": \"aaa\"}")
-		else:
-			print "WS is none"
-		time.sleep(3)
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://ec2-52-34-138-21.us-west-2.compute.amazonaws.com:8888/websocket",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
