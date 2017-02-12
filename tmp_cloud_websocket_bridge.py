3346e5b4036259551ced97f4fb063211c2eebd25
William Hass <wm.hass@gmail.com>
2017-02-12 20:10:35 -0200

    Debug

--- tmp_cloud_websocket_bridge.py
+++ tmp_cloud_websocket_bridge.py
@@ -37,25 +37,27 @@ def on_open_local(ws):
 
 websocket.enableTrace(True)
 
-local_ws = websocket.WebSocketApp("ws://127.0.0.1:8888/ws",
-                  on_message = on_message_local,
-                  on_error = on_error_local,
-                  on_close = on_close_local)
-local_ws.on_open = on_open_local
+def run_local():
+	local_ws = websocket.WebSocketApp("ws://127.0.0.1:8888/ws",
+	                  on_message = on_message_local,
+	                  on_error = on_error_local,
+	                  on_close = on_close_local)
+	local_ws.on_open = on_open_local
+	local_ws.run_forever()
+
+def run_remote():
+	remote_ws = websocket.WebSocketApp("ws://ec2-52-34-138-21.us-west-2.compute.amazonaws.com:8886/websocket",
+	                          on_message = on_message_remote,
+	                          on_error = on_error_remote,
+	                          on_close = on_close_remote)
+	remote_ws.on_open = on_open_remote
+	remote_ws.run_forever()
 
 
-
-remote_ws = websocket.WebSocketApp("ws://ec2-52-34-138-21.us-west-2.compute.amazonaws.com:8886/websocket",
-                          on_message = on_message_remote,
-                          on_error = on_error_remote,
-                          on_close = on_close_remote)
-remote_ws.on_open = on_open_remote
-
-
-wst_local = threading.Thread(target=local_ws.run_forever)
+wst_local = threading.Thread(target=run_local)
 wst_local.daemon = True
 wst_local.start()
 
-wst_remote = threading.Thread(target=remote_ws.run_forever)
+wst_remote = threading.Thread(target=run_remote)
 wst_remote.daemon = True
 wst_remote.start()
