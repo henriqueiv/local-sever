b40de06f07b25b98f29e05fee967841b92aaf97b
William Hass <wm.hass@gmail.com>
2017-02-13 14:03:56 -0200

    Created start script

--- start.sh
+++ start.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-420bits_service="~/home/pi/420bits/local-sever/420bits-service.py"
 
-if (( $(ps -ef | grep -v grep | grep $420bits_service | wc -l) > 0 ))
+if (( $(ps -aux | grep -v grep | grep "420bits-service.py" | wc -l) > 0 ))
 then
-echo "$420bits_service is running!!!"
+echo "420bits-service.py is already running"
 else
-/usr/bin/python ~/420bits/local-server/420bits-service.py > ~/420bits/logs/420bits-service.log 2>&1 &
+echo "Will start 420bits-service.py"
+/usr/bin/python "/home/pi/420bits/local-sever/420bits-service.py" > "/home/pi/420bits/logs/420bits-service.log" 2>&1 &
+echo "Did start 420bits-service.py"
+fi
+
+if (( $(ps -aux | grep -v grep | grep "420bits-webserver.py" | wc -l) > 0 ))
+then
+echo "420bits-webserver is running"
+else
+echo "Will start 420bits-service.py"
+/usr/bin/python "/home/pi/420bits/local-sever/420bits-webserver.py" > "/home/pi/420bits/logs/420bits-webserver.log" 2>&1 &
+echo "Did start 420bits-webserver.py"
 fi
