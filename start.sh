d5d8f23bd596c7907d4aba28b7bd2634465dcc3a
William Hass <wm.hass@gmail.com>
2017-02-13 14:06:47 -0200

    fixing message

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ if (( $(ps -aux | grep -v grep | grep "420bits-webserver.py" | wc -l) > 0 ))
 then
 echo "420bits-webserver is running"
 else
-echo "Will start 420bits-service.py"
+echo "Will start 420bits-webserver.py"
 /usr/bin/python "/home/pi/420bits/local-sever/420bits-webserver.py" > "/home/pi/420bits/logs/420bits-webserver.log" 2>&1 &
 echo "Did start 420bits-webserver.py"
 fi
