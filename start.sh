d93a75cf826a26fe11fad8f246ef1973973ceb30
William Hass <wm.hass@gmail.com>
2017-02-13 13:52:43 -0200

    Debugging

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-420bits_service="~/420bits/local-server/420bits-service.py"
+420bits_service="~/home/pi/420bits/local-sever/420bits-service.py"
 
-if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+if (( $(ps -ef | grep -v grep | grep $420bits_service | wc -l) > 0 ))
 then
-echo "$service is running!!!"
+echo "$420bits_service is running!!!"
 else
 /usr/bin/python ~/420bits/local-server/420bits-service.py > ~/420bits/logs/420bits-service.log 2>&1 &
 fi
