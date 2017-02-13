2e6b0d5f6c584caffb909f64e1948727941dc42d
William Hass <wm.hass@gmail.com>
2017-02-13 13:27:29 -0200

    Reloading accessory manager

--- 420bits-service.py
+++ 420bits-service.py
@@ -5,11 +5,10 @@ from app.accessory_logger import AccessoryLogger
 
 task_manager = TaskManager()
 timer_task_factory = TimerTaskFactory()
-accessory_logger = AccessoryLogger()
-
 
 while True:
-	
+	accessory_logger = AccessoryLogger()
+
 	task_manager.run_tasks(timer_task_factory.get_tasks())
 	accessory_logger.log()
 
