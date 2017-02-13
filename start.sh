7f56f93d008a7a1f2d56af22142661b46b81eb99
William Hass <wm.hass@gmail.com>
2017-02-13 13:46:08 -0200

    Added new start script

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+420bits_service="420bits-service.py"
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/usr/bin/python ~/420bits/local-server/420bits-service.py > ~/420bits/logs/420bits-service.log 2>&1 &
+fi
