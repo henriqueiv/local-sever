42a7370730dedd93f85e1ad2c18bcc225d2e266e
William Hass <wm.hass@gmail.com>
2017-02-14 11:10:20 -0200

    debugging

--- 420bits-webserver.py
+++ 420bits-webserver.py
@@ -76,13 +76,11 @@ class UpdateClientsHandler(web.RequestHandler):
     accessory_manager = AccessoryManager()    
 
     @web.asynchronous
-    def get(self, *args):
-        self.write("Hello")
+    def get(self, *args):        
+        accessories = json.dumps(self.accessory_manager.get_accessories_json())
+        update_all_clients_with_message(accessories)
+        self.write(accessories)
         self.finish()
-        # accessories = json.dumps(self.accessory_manager.get_accessories_json())
-        # update_all_clients_with_message(accessories)
-        # self.write(accessories)
-        # self.finish()
 
 
 
