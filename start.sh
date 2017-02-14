e2cfb0b5f6c69f7160fea6c7eedda49337686e1a
William Hass <wm.hass@gmail.com>
2017-02-14 00:04:14 -0200

    Updating start script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 LOGS_PATH="/home/pi/420bits/logs"
 SCRIPTS_PATH="/home/pi/420bits/local-sever"
 RESTART="0"
+STOP="0"
 
 for i in "$@"
 do
@@ -9,8 +10,8 @@ case $i in
     RESTART="1"
     ;;
 
-    -s=*|--searchpath=*)
-    SEARCHPATH="${i#*=}"
+    -s|--stop)
+    STOP="1"
     ;;
 
     -l=*|--lib=*)
@@ -29,7 +30,7 @@ done
 declare -a scripts=("420bits-service.py" "420bits-webserver.py")
 for script in "${scripts[@]}"
 do
-   	if [ "$RESTART" == "1" ]; then
+   	if [ "$RESTART" == "1" ] || [ "$STOP" == "1" ]; then
    		PID=`ps -eaf | grep $script | grep -v grep | awk '{print $2}'`
 		if [[ "" !=  "$PID" ]]; then
 		  echo "Killing $PID - Script: $script"
@@ -37,12 +38,14 @@ do
 		fi
    	fi
 
-	if (( $(ps -aux | grep -v grep | grep "$script" | wc -l) > 0 ))
-	then
-	echo "$script is already running"
-	else
-	echo "Will start $script"
-	/usr/bin/python "$SCRIPTS_PATH/$script" > "$LOGS_PATH/$script.log" 2>&1 &
-	echo "Did start $script"
-	fi
+    if [ "$STOP" == "0" ]; then
+        if (( $(ps -aux | grep -v grep | grep "$script" | wc -l) > 0 ))
+        then
+        echo "$script is already running"
+        else
+        echo "Will start $script"
+        /usr/bin/python "$SCRIPTS_PATH/$script" > "$LOGS_PATH/$script.log" 2>&1 &
+        echo "Did start $script"
+        fi
+    fi
 done
---
+++ test.sh
@@ -0,0 +1,5 @@
+FILES=./*.py
+for f in $FILES
+do
+	echo "Processing $f"
+done
