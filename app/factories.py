11605051eaade0c803a46f9f94d7f2d7de997afc
William Hass <wm.hass@gmail.com>
2017-02-15 23:02:55 -0200

    Debugging pymongo limit

--- app/factories.py
+++ app/factories.py
@@ -84,7 +84,7 @@ class AccessoryLogFactory(AbstractFactory):
 
 	def get_logs_for_api(self, from_timestamp = 0, limit = 100):
 		find_object = {"timestamp": {"$gt": float(from_timestamp)}}
-		logs = self.table.find(find_object).sort("timestamp", 1)
+		logs = self.table.find(find_object)#.sort("timestamp", 1)
 
 		max_log_timestamp = 0.0
 		logs_json = []
