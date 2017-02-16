5044d7fccbca0a9bdffad340a272de06b6fe5ae6
William Hass <wm.hass@gmail.com>
2017-02-15 23:06:25 -0200

    Trying to apply limit

--- app/factories.py
+++ app/factories.py
@@ -84,11 +84,11 @@ class AccessoryLogFactory(AbstractFactory):
 
 	def get_logs_for_api(self, from_timestamp = 0, limit = 100):
 		find_object = {"timestamp": {"$gt": float(from_timestamp)}}
-		logs = self.table.find(find_object)#.sort("timestamp", 1)
+		logs = self.table.find(find_object)[0:limit]#.sort("timestamp", 1)
 
 		max_log_timestamp = 0.0
 		logs_json = []
-		for log in logs.limit(limit):
+		for log in logs:
 			log["_id"] = str(log["_id"]) 
 			logs_json.append(log)
 			max_log_timestamp = max(max_log_timestamp, float(log["timestamp"]))
