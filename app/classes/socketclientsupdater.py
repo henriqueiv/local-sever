b8cb42a04c8227f26e72f66ac4e538caeab1aa4b
William Hass <wm.hass@gmail.com>
2017-02-16 01:26:49 -0200

    File spaces

--- app/classes/socketclientsupdater.py
+++ app/classes/socketclientsupdater.py
@@ -8,9 +8,9 @@ class SocketClientsUpdater(object):
 
 	def update_all_clients(self):
 		accessory_manager = AccessoryManager()
-	    objects = accessory_manager.get_accessories_json()
-	    self.update_all_clients_with_message(json.dumps(objects))
+		objects = accessory_manager.get_accessories_json()
+		self.update_all_clients_with_message(json.dumps(objects))
 
 	def update_all_clients_with_message(self, message):
-	    for c in self.clients:
-	        c.write_message(message)
+		for c in self.clients:
+			c.write_message(message)
