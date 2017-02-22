d417363fa0942bda35bd3a291298c996aa879fe5
William Hass <wm.hass@gmail.com>
2017-02-21 23:59:57 -0300

    Accessories

--- app/request_handlers/accessoryrequesthandler.py
+++ app/request_handlers/accessoryrequesthandler.py
@@ -3,11 +3,9 @@ from app.factories.accessoryfactory import AccessoryFactory
 import json
 
 class AccessoryRequestHandler(web.RequestHandler):
+
 	@web.asynchronous
 	def get(self, *args):
-
 		factory = AccessoryFactory()
-
-    	self.write(json.dumps(factory.get_accessories_for_api()))
-
-        self.finish()
+		self.write(json.dumps(factory.get_accessories_for_api()))
+		self.finish()
