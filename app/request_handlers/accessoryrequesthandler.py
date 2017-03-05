50c16509d4818ff1cd52575e8f4b976911cf5a7a
William Hass <wm.hass@gmail.com>
2017-03-05 19:35:13 -0300

    Validate user when reading accessories

--- app/request_handlers/accessoryrequesthandler.py
+++ app/request_handlers/accessoryrequesthandler.py
@@ -1,12 +1,15 @@
 from tornado import web
 from app.factories.accessoryfactory import AccessoryFactory
 import json
+from app.request_handlers.userauthbaserequesthandler import UserAuthBaseRequestHandler
 
-class AccessoryRequestHandler(web.RequestHandler):
+class AccessoryRequestHandler(UserAuthBaseRequestHandler):
 
 	accessory_factory = AccessoryFactory()
 
 	@web.asynchronous
 	def get(self, *args):
+		self.validate_user()
+		
 		self.write(json.dumps(self.accessory_factory.get_accessories_for_api()))
 		self.finish()
